cfad618c9dddcc205a6dd2988985ff3011bfc36e
sunbin1 <sunbin1@tal.com>
2021-05-26 20:45:03 +0800

    视图操作

--- src/main/java/com/test/tools/demo/tools/service/GitTest.java
+++ src/main/java/com/test/tools/demo/tools/service/GitTest.java
@@ -2,6 +2,6 @@ package com.test.tools.demo.tools.service;
 
 public class GitTest {
     public static void main(String[] args) {
-        System.out.println("hello world");
+        System.out.println("hello world!!");
     }
 }
