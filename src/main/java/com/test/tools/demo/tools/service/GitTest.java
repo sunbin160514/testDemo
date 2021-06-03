41c480651e4122d3947ceac6b8649fd69a874681
sunbin1 <sunbin1@tal.com>
2021-06-03 13:20:11 +0800

    testone分支

--- src/main/java/com/test/tools/demo/tools/service/GitTest.java
+++ src/main/java/com/test/tools/demo/tools/service/GitTest.java
@@ -3,5 +3,6 @@ package com.test.tools.demo.tools.service;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello world");
+        System.out.println("Good ");
     }
 }
